196643bcfcd73c9d48af9fe2b1b54630c47f6756
Mauricio Urrego <mauricio@netzstrategen.com>
2021-09-04 00:44:35 +0200

    Made PSR-4 compliant.

---
+++ src/CubeCell.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Mauriciourrego\ColorcubePhp;
+
+class CubeCell {
+  /**
+   * The color cube is made out of these cells.
+   *
+   * @var $hit_count int
+   * @var $r_acc float
+   * @var $g_acc float
+   * @var $b_acc float
+   */
+  private $hit_count;
+  private $r_acc;
+  private $g_acc;
+  private $b_acc;
+
+  public function __construct() {
+    // Count of hits (dividing the accumulators by this value gives the average color).
+    $this->hit_count = 0;
+    // Accumulators for color components.
+    $this->r_acc = 0.0;
+    $this->g_acc = 0.0;
+    $this->b_acc = 0.0;
+  }
+}
---
+++ src/LocalMaximum.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Mauriciourrego\ColorcubePhp;
+
+require 'vendor/autoload.php';
+
+class LocalMaximum {
+  // Local maxima as found during the image analysis->
+  // We need this class for ordering by cell hit count->
+  private $hit_count;
+  private $cell_index;
+  private $r;
+  private $g;
+  private $b;
+
+  public function __construct($hit_count, $cell_index, $r, $g, $b) {
+    // Hit count of the cell.
+    $this->hit_count = $hit_count;
+    // Linear index of the cell.
+    $this->cell_index = $cell_index;
+    // Average color of the cell.
+    $this->r = $r;
+    $this->g = $g;
+    $this->b = $b;
+  }
+}
